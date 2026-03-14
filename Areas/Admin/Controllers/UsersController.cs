52f29c8bbccf563cc515644560c25fdc8e5836f6
Sewar Yousef <71282585+SewarYousef@users.noreply.github.com>
2026-03-13 19:58:39 -0500

    Create UsersController.cs

---
+++ Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroupOneFlight.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ContentResult Manage()
+        {
+            return Content("Admin Area – Manage Users");
+        }
+
+        public ContentResult Rights()
+        {
+            return Content("Admin Area – Rights & Obligations");
+        }
+    }
+}
