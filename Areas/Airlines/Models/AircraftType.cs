92f6170a8e2f88ea8f2068cefcc66c0f0b9925d4
Sewar Yousef <71282585+SewarYousef@users.noreply.github.com>
2026-04-19 13:28:18 -0500

    Update AircraftType.cs

--- Areas/Airlines/Models/AircraftType.cs
+++ Areas/Airlines/Models/AircraftType.cs
@@ -2,24 +2,22 @@ namespace GroupOneFlight.Areas.Airlines.Models
 {
     public static class AircraftTypes
     {
-        public const string Boeing737 = "Boeing 737";
-        public const string Boeing787 = "Boeing 787";
-        public const string Boeing777 = "Boeing 777";
+        // Airbus A320 family
+        public const string AirbusA318 = "Airbus A318";
+        public const string AirbusA319 = "Airbus A319";
         public const string AirbusA320 = "Airbus A320";
-        public const string AirbusA380 = "Airbus A380";
-        public const string Embraer190 = "Embraer E190";
+        public const string AirbusA321 = "Airbus A321";
 
-        public static List<string> GetAll()
+        // Boeing 737 family
+        public const string Boeing737_700 = "Boeing 737-700";
+        public const string Boeing737_800 = "Boeing 737-800";
+        public const string Boeing737_900 = "Boeing 737-900";
+        public const string Boeing737Max8 = "Boeing 737 MAX 8";
+
+        public static List<string> GetAll() => new()
         {
-            return new List<string>
-            {
-                Boeing737,
-                Boeing787,
-                Boeing777,
-                AirbusA320,
-                AirbusA380,
-                Embraer190
-            };
-        }
+            AirbusA318, AirbusA319, AirbusA320, AirbusA321,
+            Boeing737_700, Boeing737_800, Boeing737_900, Boeing737Max8
+        };
     }
 }
