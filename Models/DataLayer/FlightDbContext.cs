b8376ede119b01c951a9fdf9ffd12aea3cb47a18
Sewar Yousef <71282585+SewarYousef@users.noreply.github.com>
2026-05-03 23:39:57 -0500

    Update FlightDbContext.cs

--- Models/DataLayer/FlightDbContext.cs
+++ Models/DataLayer/FlightDbContext.cs
@@ -1,32 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using GroupOneFlight.Models.DomainModels;
-using GroupOneFlight.Models.ExtensionMethods;
+using GroupOneFlight.Models.DataLayer.Configuration;
 
 namespace GroupOneFlight.Models.DataLayer
 {
     public class FlightDbContext : DbContext
     {
         public FlightDbContext(DbContextOptions<FlightDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) { }
 
-        public DbSet<Flight> Flights { get; set; }
-        public DbSet<Airline> Airlines { get; set; }
+        public DbSet<Flight>       Flights      { get; set; }
+        public DbSet<Airline>      Airlines     { get; set; }
         public DbSet<FlightOptions> FlightOptions { get; set; }
-        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Reservation>  Reservations { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            // Optional: Fluent API configurations go here
-
-            // Example (only if needed):
-            // modelBuilder.Entity<Flight>()
-            //     .HasOne(f => f.Airline)
-            //     .WithMany(a => a.Flights)
-            //     .HasForeignKey(f => f.AirlineId);
+            // Apply seed data from Configuration folder
+            modelBuilder.ApplyConfiguration(new AirlineConfig());
         }
     }
 }
